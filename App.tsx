05d1cc6b74cffe494b8f3af623f460957d6c7bc9
Mario <mario.srx99@gmail.com>
2021-03-15 00:01:05 +0100

    Implemented library

--- App.tsx
+++ App.tsx
@@ -1,30 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Modal, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Piano } from 'react-native-piano'
 
 export default function App() {
+  const [firstNote, setFirstNote] = React.useState('c4');
+  const [lastNote, setLastNote] = React.useState('c6');
+
+  let play = () => {
+    
+  }
+
+  let stop = () => {
+    
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff'
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open app to start working on your app!</Text>
-      <Button
-        title="Outline button"
-        onPress={buttonPressed}
+      <StatusBar style="auto" hidden />
+      <Piano
+        noteRange={{ first: firstNote, last: lastNote }}
+        onPlayNoteInput={play}
+        onStopNoteInput={stop}
       />
-
-      <StatusBar style="auto" />
     </View>
   );
-}
-
-let buttonPressed = () => {
 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

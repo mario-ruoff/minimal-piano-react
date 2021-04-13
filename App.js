f3500bdefe52fd4f6556e6cd4d93844a365be6a6
Mario <mario.srx99@gmail.com>
2021-04-13 15:18:07 +0200

    Reverted to javascript base project

--- App.js
+++ App.js
@@ -1,70 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useCallback, useState, useEffect } from 'react';
-import { Button, Modal, Pressable, StyleSheet, Text, View } from 'react-native';
-import Piano from './components/Piano';
-import SoundFiles from './components/SoundFiles';
-import Player from './components/Player';
-import AppLoading from 'expo-app-loading';
-import { TouchableHighlight } from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   const [appIsReady, setAppIsReady] = useState(false);
   const [firstNote, setFirstNote] = useState('c4');
   const [lastNote, setLastNote] = useState('e5');
 
-  const loadAssets = () => {
-    const sounds = Player.load(SoundFiles);
-    return Promise.all([
-      ...sounds
-    ]);
-  }
+  // const loadAssets = () => {
+  //   const sounds = Player.load(SoundFiles);
+  //   return Promise.all([
+  //     ...sounds
+  //   ]);
+  // }
 
   //splash screen when sounds not loaded
-  if (!appIsReady) {
-    return (
-      <AppLoading
-        startAsync={loadAssets}
-        onFinish={() => setAppIsReady(true)}
-        onError={console.warn}
-      />
-    )
-  }
+  // if (!appIsReady) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={loadAssets}
+  //       onFinish={() => setAppIsReady(true)}
+  //       onError={console.warn}
+  //     />
+  //   )
+  // }
   //render piano when sounds loaded
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" hidden />
+      {/* <StatusBar style="auto" hidden />
       <Piano
         noteRange={{ first: firstNote, last: lastNote }}
         onPlayNoteInput={(midiNumber) => { Player.playSound(midiNumber) }}
         onStopNoteInput={() => { }}
-      />
+      /> */}
     </View>
-    // <View style={styles.container}>
-    //   <TouchableHighlight
-    //     style={styles.button}
-    //     onPressIn={() => Player.playSound('C4')}
-    //     onPressOut={() => console.log("1 released")}
-    //   />
-    //   <TouchableHighlight
-    //     style={styles.button}
-    //     onPressIn={() => Player.playSound('D4')}
-    //     onPressOut={() => console.log("2 released")}
-    //   />
-    // </View>
   );
-
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white'
-  },
-  button: {
-    width: 50,
-    height: 50,
-    margin: 50,
-    backgroundColor: 'red',
-    
   }
-})
+});
+
+export default App;

bc7493494877a8f074dfaf5fb346100d088d7f4f
Mario <mario.srx99@gmail.com>
2021-04-14 09:04:56 +0200

    Added react-native-splash-screen testwise

--- App.js
+++ App.js
@@ -1,33 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
-import AppLoading from 'expo-app-loading';
 import Piano from './components/Piano';
 import Player from './components/Player';
 import SoundFiles from './components/SoundFiles';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
-  const [appIsReady, setAppIsReady] = useState(false);
   const [firstNote, setFirstNote] = useState('c4');
   const [lastNote, setLastNote] = useState('e5');
 
-  const loadAssets = () => {
-    const sounds = Player.load(SoundFiles);
-    return Promise.all([
-      ...sounds
-    ]);
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    console.log("hidden")
+    }, 3000);
+    // Player.load(SoundFiles);
+    
+  }, []);
 
-  //splash screen when sounds not loaded
-  if (!appIsReady) {
-    return (
-      <AppLoading
-        startAsync={loadAssets}
-        onFinish={() => setAppIsReady(true)}
-        onError={console.warn}
-      />
-    )
-  }
-  //render piano when sounds loaded
   return (
     <View style={styles.container}>
       <StatusBar hidden/>

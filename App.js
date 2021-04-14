a3e1107857af26d4e378295619bf4445ce4a4110
Mario <mario.srx99@gmail.com>
2021-04-14 09:05:18 +0200

    Revert "Added react-native-splash-screen testwise"
    This reverts commit bc7493494877a8f074dfaf5fb346100d088d7f4f.

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
 import Piano from './components/Piano';
 import Player from './components/Player';
 import SoundFiles from './components/SoundFiles';
-import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [firstNote, setFirstNote] = useState('c4');
   const [lastNote, setLastNote] = useState('e5');
 
-  useEffect(() => {
-    setTimeout(() => {
-      SplashScreen.hide();
-    console.log("hidden")
-    }, 3000);
-    // Player.load(SoundFiles);
-    
-  }, []);
+  const loadAssets = () => {
+    const sounds = Player.load(SoundFiles);
+    return Promise.all([
+      ...sounds
+    ]);
+  }
 
+  //splash screen when sounds not loaded
+  if (!appIsReady) {
+    return (
+      <AppLoading
+        startAsync={loadAssets}
+        onFinish={() => setAppIsReady(true)}
+        onError={console.warn}
+      />
+    )
+  }
+  //render piano when sounds loaded
   return (
     <View style={styles.container}>
       <StatusBar hidden/>
--- android/app/src/main/java/com/escape99/minimalpiano/MainActivity.java
+++ android/app/src/main/java/com/escape99/minimalpiano/MainActivity.java
@@ -1,11 +1,9 @@
 package com.escape99.minimalpiano;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -30,13 +28,4 @@ public class MainActivity extends ReactActivity {
       }
     };
   }
-
-  /** 
-   * Method required for react-native-splash-screen
-   */
-  @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
-        super.onCreate(savedInstanceState);
-    }
 }

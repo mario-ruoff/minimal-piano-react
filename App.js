17e11b9da50f0dfdb79879ad788ba2f2fe242881
Mario <mario.srx99@gmail.com>
2021-04-15 20:40:22 +0200

    Simplified audio player

--- App.js
+++ App.js
@@ -1,39 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
-import AppLoading from 'expo-app-loading';
 import Piano from './components/Piano';
-import Player from './components/Player';
 import SoundFiles from './components/SoundFiles';
+import { Audio } from 'expo-av'
 
 const App = () => {
-  const [appIsReady, setAppIsReady] = useState(false);
   const [firstNote, setFirstNote] = useState('c4');
   const [lastNote, setLastNote] = useState('e5');
 
-  const loadAssets = async () => {
-    const sounds = Player.load(SoundFiles);
-    return Promise.all([
-      ...sounds
-    ]);
-  }
-
-  //splash screen when sounds not loaded
-  if (!appIsReady) {
-    return (
-      <AppLoading
-        startAsync={loadAssets}
-        onFinish={() => setAppIsReady(true)}
-        onError={console.warn}
-      />
-    )
-  }
-  //render piano when sounds loaded
   return (
     <View style={styles.container}>
-      <StatusBar hidden/>
+      <StatusBar hidden />
       <Piano
         noteRange={{ first: firstNote, last: lastNote }}
-        onPlayNoteInput={(midiNumber) => { Player.playSound(midiNumber) }}
+        onPlayNoteInput={async (midiNumber) => {
+          const { sound } = await Audio.Sound.createAsync(SoundFiles[midiNumber]);
+          const status = await sound.playAsync();
+          setTimeout(() => {
+            sound.unloadAsync();
+          }, status.playableDurationMillis);
+        }}
         onStopNoteInput={() => { }}
       />
     </View>
